a5dd29142ad7294304ec87aa0b0e9bbbefd8bd51
liza <lizashatuzhilkina@gmail.com>
2022-05-07 21:02:11 +0300

    new examples

--- Example005_If/Program.cs
+++ Example005_If/Program.cs
@@ -1,9 +1,9 @@
 ﻿Console.Write("Введите имя пользователя: ");
 string username = Console.ReadLine();
 
-if(username.ToLower() == "маша")
+if(username.ToLower() == "илюша")
 {
-     Console.WriteLine("Ура, это же Маша!");
+     Console.WriteLine("Ура, это же Илюша!");
 }
 else
 {
